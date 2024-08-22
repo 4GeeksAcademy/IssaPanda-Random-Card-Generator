3cb5acd355a8d515f9537c83b2c97551794b2c30
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-08-21 20:34:02 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let concatenatedString = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  
+  return concatenatedString;
+}
+
+window.onload = function() {
+  let excuse = generateExcuse();
+  document.querySelector("#excuse").innerHTML = excuse;
+};
