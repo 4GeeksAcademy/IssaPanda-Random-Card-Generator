e251b6b6ae115060a3bdcbeb4f8acc42bfe2c54b
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-08-23 22:19:10 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,29 +4,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
 ];
+let suits = ["♦", "♥", "♠", "♣"];
 
-function generateExcuse() {
-  let randomWho = who[Math.floor(Math.random() * who.length)];
-  let randomAction = action[Math.floor(Math.random() * action.length)];
-  let randomWhat = what[Math.floor(Math.random() * what.length)];
-  let randomWhen = when[Math.floor(Math.random() * when.length)];
+window.onload = function randomCard() {
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
 
-  let concatenatedString = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
-  
-  return concatenatedString;
-}
+  let myCard = document.querySelector("#my-card");
+  myCard.innerHTML = `
+    <h2 class="col-5 text-start">${suit}</h2>
+    <h2 class="col-5">${number}</h2>
+    <h2 class="col-5 text-end">${suit}</h2>
+  `;
 
-window.onload = function() {
-  let excuse = generateExcuse();
-  document.querySelector("#excuse").innerHTML = excuse;
+  if (suit === "♦" || suit === "♥") {
+    myCard.style.color = "red";
+  } else {
+    myCard.style.color = "black";
+  }
 };
